559e221e0704b59083e14b9c2498b25c3ebca9f9
lgithm <113286830+lgithm@users.noreply.github.com>
2023-02-14 15:41:50 +0400

    Update story-a7438b6fccb13a7c8870.js

--- story-a7438b6fccb13a7c8870.js
+++ story-a7438b6fccb13a7c8870.js
@@ -1,1 +1,177 @@
-_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[12],{"8dqH":function(e,t,r){(window.__NEXT_P=window.__NEXT_P||[]).push(["/story",function(){return r("FyHc")}])},FyHc:function(e,t,r){"use strict";r.r(t),r.d(t,"default",(function(){return l}));var s=r("nKUr"),o=r("g4pe"),a=r.n(o),n=r("YFqc"),i=r.n(n),c=r("ZHh6");function l(){return Object(s.jsxs)(s.Fragment,{children:[Object(s.jsx)(a.a,{children:Object(s.jsx)("title",{children:"CP: Zero to Hero | Story"})}),Object(s.jsxs)("div",{className:"bg-gray-100 min-h-screen px-4",children:[Object(s.jsx)(c.a,{}),Object(s.jsxs)("div",{className:"-mt-16 pt-24 pb-8 max-w-prose mx-auto",children:["It was March 32nd, 2021. It was 1 am, and I sat at my desk, just remembering that I had promised someone I would make a course by April 2021. I realized that I had made no progress at all, so I got to work.",Object(s.jsx)("br",{}),Object(s.jsx)("br",{}),"I had some web development experience, but there will still things that were challenging for me. For one, I have never worked with authentication before. Thankfully, Firebase made it easy. I got it to work after about 2 hours and then I went to sleep.",Object(s.jsx)("br",{}),Object(s.jsx)("br",{}),"I had a hard time sleeping due to the tight deadline, and at around 7am I couldn't sleep any more. I worked on the backend infrastructure for the assessment for a few hours. In the afternoon, I started to come up with more problems and implement them into both the frontend and the backend.",Object(s.jsx)("br",{}),Object(s.jsx)("br",{}),'Originally, I planned to have a total of 10 problems, but around 4pm I was only done with 8. I decided to try and make the UI look prettier. I also had to come up with a favicon and a cover image. I couldn\'t really think of anything good for the favicon so I just used favicon.io and put in the word "CP". For the cover, I just put my programs for ',Object(s.jsx)("a",{href:"https://codeforces.com/contest/4/problem/A",className:"text-blue-500 hover:text-blue-600 transition",children:"Watermelon"})," and ",Object(s.jsx)("a",{href:"https://codeforces.com/contest/71/problem/A",className:"text-blue-500 hover:text-blue-600 transition",children:"Way Too Long Words"})," into Photoshop and used some 3D transformations.",Object(s.jsx)("br",{}),Object(s.jsx)("br",{}),"Later at night, I started sending the site for people to test and I started promoting it. There were a few production bugs, which were embarassing. For the last problem, I didn't configure the problem correctly and it actually made the problem possible to solve, which was not the intention. People were solving the problem and asking why their scores didn't update. I've also heard some issues with authentication for firefox but if you don't use Chrome then screw you.",Object(s.jsx)("br",{}),Object(s.jsx)("br",{}),"Overall, things worked out smoothly. People were pre-registering and failing to pass. Some people soon realized that the last 3 problems were impossible, while others were emailing me, asking for more chances.",Object(s.jsx)("br",{}),Object(s.jsx)("br",{}),"Fast forward to 4/2, I decided it was time to send out ",Object(s.jsx)(i.a,{href:"/result",children:Object(s.jsx)("a",{className:"text-blue-500 hover:text-blue-600 transition",children:"decision letters"})})," to the 7000+ people who have pre-registered. I tried the mailgun API at night and I started sending out emails with it on 4/3 in the afternoon. I didn't use the API efficiently and it took over 2 hours to send the emails but as expected, people got rickrolled!",Object(s.jsx)("br",{}),Object(s.jsx)("br",{}),"The next day, I decided that it was a good time to explicitly declare this course a joke and also to present the solutions:",Object(s.jsx)("br",{}),Object(s.jsx)("br",{}),"Problem 1:",Object(s.jsx)("br",{}),"6",Object(s.jsx)("br",{}),Object(s.jsx)("br",{}),"Problem 2:",Object(s.jsx)("br",{}),"25",Object(s.jsx)("br",{}),Object(s.jsx)("br",{}),"Problem 3:",Object(s.jsx)("br",{}),"One possible solution is:",Object(s.jsx)("br",{}),Object(s.jsxs)("code",{children:[".#.#.",Object(s.jsx)("br",{}),".....",Object(s.jsx)("br",{}),"#.#.#",Object(s.jsx)("br",{}),".....",Object(s.jsx)("br",{}),".#.#."]}),Object(s.jsx)("br",{}),Object(s.jsx)("br",{}),"Problem 4:",Object(s.jsx)("br",{}),"0, 0, 0",Object(s.jsx)("br",{}),Object(s.jsx)("br",{}),"Problem 5:",Object(s.jsx)("br",{}),Object(s.jsxs)("code",{children:[".#.",Object(s.jsx)("br",{}),".#.",Object(s.jsx)("br",{}),"###"]}),Object(s.jsx)("br",{}),Object(s.jsx)("br",{}),"Problem 6:",Object(s.jsx)("br",{}),"Notice that you gain 0.5 extra regions if you capture a border cell and 0 otherwise. The strategy is then obvious, and it's also obvious that the best both of you can do is tie.",Object(s.jsx)("br",{}),Object(s.jsx)("br",{}),"Problem 7:",Object(s.jsx)("br",{}),"It's always possible for Bob to achieve the goal. See ",Object(s.jsx)("a",{href:"https://www.reddit.com/r/mathriddles/comments/2v6eaj/doubling_and_adding_1/",className:"text-blue-500 hover:text-blue-700 transition",children:"this"}),".",Object(s.jsx)("br",{}),Object(s.jsx)("br",{}),"Problem 8:",Object(s.jsx)("br",{}),"By a simple invariant argument, it's impossible to have 1 stone left.",Object(s.jsx)("br",{}),Object(s.jsx)("br",{}),"By the time of this writing, there have been 8474 pre-registrations. The GitHub repository for this website is ",Object(s.jsx)("a",{href:"https://github.com/tmwilliamlin168/course-for-fools",className:"text-blue-500 hover:text-blue-700 transition",children:"here"}),". Thanks to CJ Quines and Cynthia Du for help with the website. Thanks to Nathan Wang for providing the template for the landing page. And most importantly, thanks to all of you who fell for the prank :)"]})]})]})}},ZHh6:function(e,t,r){"use strict";r.d(t,"a",(function(){return c}));var s=r("nKUr"),o=r("q1tI"),a=r("YFqc"),n=r.n(a),i=[{label:"Pre-Register",url:"/register"},{label:"Contact",url:"mailto:course@williamlin.io"},{label:"Our Story",url:"/story"}];function c(){var e=Object(o.useState)(!1),t=e[0],r=e[1];return Object(s.jsxs)(s.Fragment,{children:[Object(s.jsxs)("div",{className:"bg-white shadow fixed inset-x-0 top-0 z-10",children:[Object(s.jsx)("div",{className:"max-w-7xl mx-auto px-4 sm:px-6 lg:px-8",children:Object(s.jsxs)("div",{className:"flex justify-between h-16",children:[Object(s.jsxs)("div",{className:"flex",children:[Object(s.jsx)(n.a,{href:"/",children:Object(s.jsx)("a",{className:"bg-clip-text text-transparent flex-shrink-0 inline-flex items-center text-xl font-bold bg-gradient-to-r from-blue-300 to-blue-600",children:"Zero to Hero"})}),Object(s.jsx)("div",{className:"hidden sm:ml-8 sm:flex space-x-8",children:i.map((function(e){return e.url?Object(s.jsx)(n.a,{href:e.url,children:Object(s.jsx)("a",{className:"inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out",children:e.label})},e.label):Object(s.jsx)("a",{className:"inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out",title:"Coming soon!",children:e.label},e.label)}))})]}),Object(s.jsx)("div",{className:"-mr-2 flex items-center sm:hidden",children:Object(s.jsxs)("button",{className:"inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out","aria-label":"Main menu","aria-expanded":"false",onClick:function(){return r(!t)},children:[!t&&Object(s.jsx)("svg",{className:"h-6 w-6",fill:"none",viewBox:"0 0 24 24",stroke:"currentColor",children:Object(s.jsx)("path",{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M4 6h16M4 12h16M4 18h16"})}),t&&Object(s.jsx)("svg",{className:"h-6 w-6",fill:"none",viewBox:"0 0 24 24",stroke:"currentColor",children:Object(s.jsx)("path",{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M6 18L18 6M6 6l12 12"})})]})})]})}),Object(s.jsx)("div",{className:(t?"block":"hidden")+" sm:hidden",children:Object(s.jsx)("div",{className:"pt-2 pb-3 space-y-1",children:i.map((function(e){return Object(s.jsx)(n.a,{href:e.url,children:Object(s.jsx)("a",{className:"block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out",children:e.label})},e.label)}))})})]}),Object(s.jsx)("div",{className:"m-16"})]})}}},[["8dqH",0,2,1,3]]]);
+_N_E = (window.webpackJsonp_N_E = window.webpackJsonp_N_E || []).push([
+	[12], {
+		"8dqH": function(e, t, r) {
+			(window.__NEXT_P = window.__NEXT_P || []).push(["/story", function() {
+				return r("FyHc")
+			}])
+		},
+		FyHc: function(e, t, r) {
+			"use strict";
+			r.r(t), r.d(t, "default", (function() {
+				return l
+			}));
+			var s = r("nKUr"),
+				o = r("g4pe"),
+				a = r.n(o),
+				n = r("YFqc"),
+				i = r.n(n),
+				c = r("ZHh6");
+
+			function l() {
+				return Object(s.jsxs)(s.Fragment, {
+					children: [Object(s.jsx)(a.a, {
+						children: Object(s.jsx)("title", {
+							children: "CP: Zero to Hero | Story"
+						})
+					}), Object(s.jsxs)("div", {
+						className: "bg-gray-100 min-h-screen px-4",
+						children: [Object(s.jsx)(c.a, {}), Object(s.jsxs)("div", {
+							className: "-mt-16 pt-24 pb-8 max-w-prose mx-auto",
+							children: ["It was March 32nd, 2021. It was 1 am, and I sat at my desk, just remembering that I had promised someone I would make a course by April 2021. I realized that I had made no progress at all, so I got to work.", Object(s.jsx)("br", {}), Object(s.jsx)("br", {}), "I had some web development experience, but there will still things that were challenging for me. For one, I have never worked with authentication before. Thankfully, Firebase made it easy. I got it to work after about 2 hours and then I went to sleep.", Object(s.jsx)("br", {}), Object(s.jsx)("br", {}), "I had a hard time sleeping due to the tight deadline, and at around 7am I couldn't sleep any more. I worked on the backend infrastructure for the assessment for a few hours. In the afternoon, I started to come up with more problems and implement them into both the frontend and the backend.", Object(s.jsx)("br", {}), Object(s.jsx)("br", {}), 'Originally, I planned to have a total of 10 problems, but around 4pm I was only done with 8. I decided to try and make the UI look prettier. I also had to come up with a favicon and a cover image. I couldn\'t really think of anything good for the favicon so I just used favicon.io and put in the word "CP". For the cover, I just put my programs for ', Object(s.jsx)("a", {
+								href: "https://codeforces.com/contest/4/problem/A",
+								className: "text-blue-500 hover:text-blue-600 transition",
+								children: "Watermelon"
+							}), " and ", Object(s.jsx)("a", {
+								href: "https://codeforces.com/contest/71/problem/A",
+								className: "text-blue-500 hover:text-blue-600 transition",
+								children: "Way Too Long Words"
+							}), " into Photoshop and used some 3D transformations.", Object(s.jsx)("br", {}), Object(s.jsx)("br", {}), "Later at night, I started sending the site for people to test and I started promoting it. There were a few production bugs, which were embarassing. For the last problem, I didn't configure the problem correctly and it actually made the problem possible to solve, which was not the intention. People were solving the problem and asking why their scores didn't update. I've also heard some issues with authentication for firefox but if you don't use Chrome then screw you.", Object(s.jsx)("br", {}), Object(s.jsx)("br", {}), "Overall, things worked out smoothly. People were pre-registering and failing to pass. Some people soon realized that the last 3 problems were impossible, while others were emailing me, asking for more chances.", Object(s.jsx)("br", {}), Object(s.jsx)("br", {}), "Fast forward to 4/2, I decided it was time to send out ", Object(s.jsx)(i.a, {
+								href: "/result",
+								children: Object(s.jsx)("a", {
+									className: "text-blue-500 hover:text-blue-600 transition",
+									children: "decision letters"
+								})
+							}), " to the 7000+ people who have pre-registered. I tried the mailgun API at night and I started sending out emails with it on 4/3 in the afternoon. I didn't use the API efficiently and it took over 2 hours to send the emails but as expected, people got rickrolled!", Object(s.jsx)("br", {}), Object(s.jsx)("br", {}), "The next day, I decided that it was a good time to explicitly declare this course a joke and also to present the solutions:", Object(s.jsx)("br", {}), Object(s.jsx)("br", {}), "Problem 1:", Object(s.jsx)("br", {}), "6", Object(s.jsx)("br", {}), Object(s.jsx)("br", {}), "Problem 2:", Object(s.jsx)("br", {}), "25", Object(s.jsx)("br", {}), Object(s.jsx)("br", {}), "Problem 3:", Object(s.jsx)("br", {}), "One possible solution is:", Object(s.jsx)("br", {}), Object(s.jsxs)("code", {
+								children: [".#.#.", Object(s.jsx)("br", {}), ".....", Object(s.jsx)("br", {}), "#.#.#", Object(s.jsx)("br", {}), ".....", Object(s.jsx)("br", {}), ".#.#."]
+							}), Object(s.jsx)("br", {}), Object(s.jsx)("br", {}), "Problem 4:", Object(s.jsx)("br", {}), "0, 0, 0", Object(s.jsx)("br", {}), Object(s.jsx)("br", {}), "Problem 5:", Object(s.jsx)("br", {}), Object(s.jsxs)("code", {
+								children: [".#.", Object(s.jsx)("br", {}), ".#.", Object(s.jsx)("br", {}), "###"]
+							}), Object(s.jsx)("br", {}), Object(s.jsx)("br", {}), "Problem 6:", Object(s.jsx)("br", {}), "Notice that you gain 0.5 extra regions if you capture a border cell and 0 otherwise. The strategy is then obvious, and it's also obvious that the best both of you can do is tie.", Object(s.jsx)("br", {}), Object(s.jsx)("br", {}), "Problem 7:", Object(s.jsx)("br", {}), "It's always possible for Bob to achieve the goal. See ", Object(s.jsx)("a", {
+								href: "https://www.reddit.com/r/mathriddles/comments/2v6eaj/doubling_and_adding_1/",
+								className: "text-blue-500 hover:text-blue-700 transition",
+								children: "this"
+							}), ".", Object(s.jsx)("br", {}), Object(s.jsx)("br", {}), "Problem 8:", Object(s.jsx)("br", {}), "By a simple invariant argument, it's impossible to have 1 stone left.", Object(s.jsx)("br", {}), Object(s.jsx)("br", {}), "By the time of this writing, there have been 8474 pre-registrations. The GitHub repository for this website is ", Object(s.jsx)("a", {
+								href: "https://github.com/tmwilliamlin168/course-for-fools",
+								className: "text-blue-500 hover:text-blue-700 transition",
+								children: "here"
+							}), ". Thanks to CJ Quines and Cynthia Du for help with the website. Thanks to Nathan Wang for providing the template for the landing page. And most importantly, thanks to all of you who fell for the prank :)"]
+						})]
+					})]
+				})
+			}
+		},
+		ZHh6: function(e, t, r) {
+			"use strict";
+			r.d(t, "a", (function() {
+				return c
+			}));
+			var s = r("nKUr"),
+				o = r("q1tI"),
+				a = r("YFqc"),
+				n = r.n(a),
+				i = [{
+					label: "Pre-Register",
+					url: "/register"
+				}, {
+					label: "Contact",
+					url: "mailto:course@williamlin.io"
+				}, {
+					label: "Our Story",
+					url: "/story"
+				}];
+
+			function c() {
+				var e = Object(o.useState)(!1),
+					t = e[0],
+					r = e[1];
+				return Object(s.jsxs)(s.Fragment, {
+					children: [Object(s.jsxs)("div", {
+						className: "bg-white shadow fixed inset-x-0 top-0 z-10",
+						children: [Object(s.jsx)("div", {
+							className: "max-w-7xl mx-auto px-4 sm:px-6 lg:px-8",
+							children: Object(s.jsxs)("div", {
+								className: "flex justify-between h-16",
+								children: [Object(s.jsxs)("div", {
+									className: "flex",
+									children: [Object(s.jsx)(n.a, {
+										href: "/",
+										children: Object(s.jsx)("a", {
+											className: "bg-clip-text text-transparent flex-shrink-0 inline-flex items-center text-xl font-bold bg-gradient-to-r from-blue-300 to-blue-600",
+											children: "Zero to Hero"
+										})
+									}), Object(s.jsx)("div", {
+										className: "hidden sm:ml-8 sm:flex space-x-8",
+										children: i.map((function(e) {
+											return e.url ? Object(s.jsx)(n.a, {
+												href: e.url,
+												children: Object(s.jsx)("a", {
+													className: "inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out",
+													children: e.label
+												})
+											}, e.label) : Object(s.jsx)("a", {
+												className: "inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out",
+												title: "Coming soon!",
+												children: e.label
+											}, e.label)
+										}))
+									})]
+								}), Object(s.jsx)("div", {
+									className: "-mr-2 flex items-center sm:hidden",
+									children: Object(s.jsxs)("button", {
+										className: "inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out",
+										"aria-label": "Main menu",
+										"aria-expanded": "false",
+										onClick: function() {
+											return r(!t)
+										},
+										children: [!t && Object(s.jsx)("svg", {
+											className: "h-6 w-6",
+											fill: "none",
+											viewBox: "0 0 24 24",
+											stroke: "currentColor",
+											children: Object(s.jsx)("path", {
+												strokeLinecap: "round",
+												strokeLinejoin: "round",
+												strokeWidth: "2",
+												d: "M4 6h16M4 12h16M4 18h16"
+											})
+										}), t && Object(s.jsx)("svg", {
+											className: "h-6 w-6",
+											fill: "none",
+											viewBox: "0 0 24 24",
+											stroke: "currentColor",
+											children: Object(s.jsx)("path", {
+												strokeLinecap: "round",
+												strokeLinejoin: "round",
+												strokeWidth: "2",
+												d: "M6 18L18 6M6 6l12 12"
+											})
+										})]
+									})
+								})]
+							})
+						}), Object(s.jsx)("div", {
+							className: (t ? "block" : "hidden") + " sm:hidden",
+							children: Object(s.jsx)("div", {
+								className: "pt-2 pb-3 space-y-1",
+								children: i.map((function(e) {
+									return Object(s.jsx)(n.a, {
+										href: e.url,
+										children: Object(s.jsx)("a", {
+											className: "block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out",
+											children: e.label
+										})
+									}, e.label)
+								}))
+							})
+						})]
+					}), Object(s.jsx)("div", {
+						className: "m-16"
+					})]
+				})
+			}
+		}
+	},
+	[
+		["8dqH", 0, 2, 1, 3]
+	]
+]);
